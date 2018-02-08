e647caf38267e7c48ed6284ce0138220a31b0671
NoahReifsnyder <noahreifsnyder@Noahs-MacBook-Pro.local>
2018-02-08 02:42:19 -0500

    fix

--- test.py
+++ test.py
@@ -169,6 +169,7 @@ def wait(key):
     global mylocks
     while not key in mylocks or not mylocks[key]==iplen():
         print(iplen(),key,mylocks)
+        print((not key in mylocks),(not mylocks[key]==iplen())
         pass
 
 def gencmds(slist):
