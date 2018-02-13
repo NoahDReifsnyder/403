c80a0dcf12d17a5d8b20212d59e7498a498f9cde
NoahReifsnyder <noahreifsnyder@dyn135147.cc.lehigh.edu>
2018-02-13 18:48:18 -0500

    broken

--- test.py
+++ test.py
@@ -227,12 +227,12 @@ def wait(key,slist,id):
     global iglist
     key=str(key)
     dt=datetime.now()
-    to=1
+    to=5
     while not key in mylocks or not mylocks[key]==iplen():
         td=datetime.now()-dt
         ts=td.total_seconds()
         if ts>to:
-            to=to*2
+            to=to/2
             iglist.append(id)
             unlock(key,slist)
             time.sleep(.1)
