c2274e20a39b0cef697f1cb2501f1045f9aaccf9
NoahReifsnyder <noahreifsnyder@dyn135147.cc.lehigh.edu>
2018-02-14 13:42:28 -0500

    4 nodes

--- test.py
+++ test.py
@@ -75,6 +75,7 @@ def start_up():
     n=10 #number of nodes, plus a few extra for safety
     partition={}
     for ip in iplist:
+        print(ip)
         i=PORT_NUMBER
         flag=True
         while i < PORT_NUMBER+n and flag:
