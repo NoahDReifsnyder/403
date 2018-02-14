deec224397af4033a3d7e26e9cffbedcc3504ab1
NoahReifsnyder <noahreifsnyder@dyn135147.cc.lehigh.edu>
2018-02-14 14:18:28 -0500

    new connection model

--- test.py
+++ test.py
@@ -75,6 +75,8 @@ def start_up():
     n=10 #number of nodes, plus a few extra for safety
     partition={}
     for ip in iplist:
+        if ip==get_ip_address():
+            continue
         print(ip)
         i=PORT_NUMBER
         flag=True
