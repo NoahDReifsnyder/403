3327f93a9785c7c42011ac2a33234222d37b00f2
NoahReifsnyder <noahreifsnyder@Noahs-MacBook-Pro.local>
2018-02-14 22:53:41 -0500

    new connection model

--- test.py
+++ test.py
@@ -188,7 +188,7 @@ def locked(k,s,id):
         LOCLOCL[k].acquire()
         for soc in remlocks:
             while k not in remlocks[soc]:
-                print('here2')
+                #print('here2')
                 pass
     remlocks[s].append(k)
     LOCLOCL[k].release()
