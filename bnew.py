81eaf6a515b37898ec600efc1b98d213a1813bfb
NoahReifsnyder <noahreifsnyder@dyn132147.cc.lehigh.edu>
2018-02-19 16:58:40 -0500

    close

--- bnew.py
+++ bnew.py
@@ -357,7 +357,7 @@ def parse(mssg,s):
 def wait(key,id):
     key=str(key)
     dt=datetime.now()
-    while not mylocks[key]==iplen():
+    while not mylocks[key]>=iplen():
         tn=datetime.now()
         td=tn-dt
         ts=td.total_seconds()
