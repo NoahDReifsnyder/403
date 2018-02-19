f733e8ef4e4ed0cec8b442de6021d2e869eb19c1
NoahReifsnyder <noahreifsnyder@dyn132147.cc.lehigh.edu>
2018-02-19 18:16:50 -0500

    close

--- bnew.py
+++ bnew.py
@@ -418,6 +418,7 @@ def cmds(i):
     else:
         value=get(key)
         print(i,"Get:",key,value)
+    print(key,a)
     unlock(key)
     #print("Command",i,"of",num)
     td=((datetime.now())-dt).total_seconds()
