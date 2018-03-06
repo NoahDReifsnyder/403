42de2ccf8348e9daed193b7abfbd2129df95d26c
NoahReifsnyder <noahreifsnyder@dyn133189.cc.lehigh.edu>
2018-03-06 11:31:14 -0500

    Final

---
+++ NWL.py
@@ -0,0 +1,1 @@
+#Put Network Layer Code here for future use. Simple layout, can modify
--- new.py
+++
@@ -1,7 +0,0 @@
-import json
-iplist=['172.31.47.97','172.31.37.86','172.31.43.198','172.31.36.171']
-num=100
-keyrange=100
-data={"ip":iplist,"ops":num,"keyrange":keyrange}
-with open('config.txt', 'w') as outfile:
-    json.dump(data, outfile)
